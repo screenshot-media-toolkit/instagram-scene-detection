82b8c2830b339c306fe10b93597f1a13cbd6e5ff
alan910127 <alan469357@gmail.com>
2023-02-19 17:33:24 +0800

    🎉 Scaffold new project

---
+++ src/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import logging
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
